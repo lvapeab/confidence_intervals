29b82f054beb00dc1d79e9e818bf109a259f83a3
lvapeab <lvapeab@gmail.com>
2017-09-22 11:51:05 +0200

    Update for NMT-Keras logging

--- process_inmt_file.sh
+++ process_inmt_file.sh
@@ -4,4 +4,4 @@ destdir=`dirname $1`
 base=`basename $1`
 basename="${base%.*}"
 
-cat $1 |grep WSR |awk '{if (NF > 10) {print $6" "$16}}' |awk 'BEGIN{FS="."}{print "0."$2" 0."$4}' > ${destdir}/${basename}.scores
+cat $1 |grep WSR |awk '{if (NF > 10) {print $7" "$17}}' |awk 'BEGIN{FS="."}{print "0."$2" 0."$4}' > ${destdir}/${basename}.scores
